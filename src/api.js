a38124ce2577e193fa5392402c021887e200cd5e
Renkel <guirenkel@gmail.com>
2023-05-24 11:19:43 -0300

    feat: adds a session id to use as identifier on the API

--- src/api.js
+++ src/api.js
@@ -1,14 +1,40 @@
 import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
 const BASE_URL = 'http://localhost:5050/'
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  timeout: 5000,
+});
 
 const paths = {
   files: 'api/files',
   users: 'api/users'
-}
+};
+
+(
+  () => {
+    let sessionIdentifier = localStorage.getItem('session-id');
+    if (!sessionIdentifier) {
+      const sessionId = uuidv4();
+      localStorage.setItem('session-id', sessionId);
+      sessionIdentifier = sessionId
+    }
+    axiosInstance.interceptors.request.use(config => {
+      config.headers = {
+        ...config.headers,
+        session_id: sessionIdentifier,
+      };
+      return config;
+    }, error => {
+      return Promise.reject(error);
+    });
+  }
+)()
 
 export async function uploadCSV(formData) {
   try {
-    const response = await axios.post(`${BASE_URL}${paths.files}`, formData, {
+    const response = await axiosInstance.post(`${paths.files}`, formData, {
       headers: {
         'Content-Type': 'multipart/form-data',
       },
@@ -22,7 +48,7 @@ export async function uploadCSV(formData) {
 
 export async function searchUsers(query) {
   try {
-    const response = await axios.get(`${BASE_URL}${paths.users}`, { params: { q: query}});
+    const response = await axiosInstance.get(`${paths.users}`, { params: { q: query } });
     return response.data;
   } catch (error) {
     throw new Error('API request failed');
