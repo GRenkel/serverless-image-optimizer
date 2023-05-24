b7847169c4aa428a36e18da50d37391e55621492
Renkel <guirenkel@gmail.com>
2023-05-24 20:18:38 -0300

    refac: improves api error handling

--- src/services/api.js
+++ src/services/api.js
@@ -1,42 +1,35 @@
 import axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
 const BASE_URL = 'http://localhost:5050/'
-
-const axiosInstance = axios.create({
-  baseURL: BASE_URL,
-  timeout: 5000,
-});
-
 const paths = {
   files: 'api/files',
   users: 'api/users'
 };
 
-export function configureApiInterceptors() {
+export function configureApiAxiosInstance() {
   let sessionIdentifier = localStorage.getItem('session-id');
   if (!sessionIdentifier) {
     const sessionId = uuidv4();
     localStorage.setItem('session-id', sessionId);
     sessionIdentifier = sessionId;
   }
-
-  axiosInstance.interceptors.request.use(config => {
-    config.headers = {
-      ...config.headers,
-      session_id: sessionIdentifier,
-    };
-    return config;
-  }, error => {
-    return Promise.reject(error);
+  const axiosInstance = axios.create({
+    baseURL: BASE_URL,
+    timeout: 5000,
+    headers: {
+      'session_id': sessionIdentifier
+    }
   });
+
+  return axiosInstance;
 }
 
-configureApiInterceptors();
+const axiosInstance = configureApiAxiosInstance();
 
-function handleApiError(err) {
+export function handleApiError(err) {
   const { data: { uiMessage, error } } = err.response;
   console.error('API - ERROR:', error);
-  throw {uiMessage}
+  throw new Error(uiMessage)
 }
 
 export async function uploadCSV(formData) {
