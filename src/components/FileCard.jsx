22b3255f3bd2e6b342f19298187b5d23f72159f6
Renkel <guirenkel@gmail.com>
2023-06-05 13:05:44 -0300

    feat: adds FileCard to FilesList

---
+++ src/components/FileCard.jsx
@@ -0,0 +1,26 @@
+import { Avatar, Card } from "antd"
+import { FileOutlined, DeleteOutlined, CloudDownloadOutlined, ContainerTwoTone } from '@ant-design/icons';
+
+export function FileCard({ id, name, size, sizeUnit }) {
+  return (
+    <Card
+      hoverable
+      style={{ width: 250, margin: '10px' }}
+      key={id}
+    >
+      <div style={{ display: 'flex', gap: 5, alignItems: 'center' }}>
+        <Avatar icon={<FileOutlined />} />
+        <span data-testid="span-name">{name}</span>
+      </div>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: 5, paddingTop: 10 }}>
+        <div style={{ display: 'flex', width: '100%', justifyContent: 'space-around' }}>
+          <CloudDownloadOutlined style={{ fontSize: 25, color: 'red' }} />
+          <DeleteOutlined style={{ fontSize: 25, color: 'red'}} />
+        </div>
+        <div style={{ alignSelf: 'center' }}>
+          <ContainerTwoTone /> {size.toFixed(2)}{' '}{sizeUnit}
+        </div>
+      </div>
+    </Card>
+  )
+}
--- src/components/FilesList.jsx
+++ src/components/FilesList.jsx
@@ -1,7 +1,7 @@
-import { Avatar, Card, Empty } from "antd";
-import { UserOutlined, EnvironmentTwoTone, HeartTwoTone } from '@ant-design/icons';
+import { Empty } from "antd";
 import { translate } from '../locales/translator'
 import { FileSkeletonCard } from "./FileSkeletonCard";
+import { FileCard } from "./FileCard";
 
 export function FilesList({ fileData, isLoading }) {
 
@@ -19,26 +19,7 @@ export function FilesList({ fileData, isLoading }) {
 
   return (
     <div style={{ display: 'flex', flex: 1, flexWrap: 'wrap', justifyContent: 'center' }}>
-      {fileData.map((row) => (
-        <Card
-          hoverable
-          style={{ width: 250, margin: '10px' }}
-          key={row.id}
-        >
-          <div style={{ display: 'flex', gap: 5, alignItems: 'center' }}>
-            <Avatar icon={<UserOutlined />} />
-            <span data-testid="span-name">{row.name}</span>
-          </div>
-          <div style={{ display: 'flex', flexDirection: 'column', gap: 5, paddingTop: 10 }}>
-            <div>
-              <EnvironmentTwoTone /> {row.city}, {row.country}
-            </div>
-            <div>
-              <HeartTwoTone twoToneColor="#eb2f96" /> {row.favorite_sport}
-            </div>
-          </div>
-        </Card>
-      ))}
+      {fileData.map((row) => <FileCard  {...row}/>)}
     </div>
   )
 }
