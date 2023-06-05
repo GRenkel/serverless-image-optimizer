bd0ac2b82cf189fae3370304dde2e09568866f63
Renkel <guirenkel@gmail.com>
2023-06-05 12:46:39 -0300

    fix: changes how AWS credentials are returned

--- src/services/apis/config/awsCredentials.js
+++ src/services/apis/config/awsCredentials.js
@@ -1,10 +1,9 @@
-import { StaticCredentialsProvider } from '@aws-sdk/credential-providers';
 
 export const getAWSCredentials = () => {
   // WARNING!!!
   // don't do it! fetch temporary credentials from your API or use cognito instead!
-  return new StaticCredentialsProvider({
+  return {
     accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
-  })
+  }
 }
