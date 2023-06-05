decf2f3e68b2f63d640664964bc5fd3902238178
Renkel <guirenkel@gmail.com>
2023-06-05 12:47:29 -0300

    feat: adds listBucketObjectsCommand

--- src/services/apis/config/awsS3.js
+++ src/services/apis/config/awsS3.js
@@ -1,4 +1,4 @@
-import { HeadBucketCommand, S3Client } from "@aws-sdk/client-s3";
+import { CreateBucketCommand, HeadBucketCommand, ListObjectsCommand, S3Client } from "@aws-sdk/client-s3";
 import { getSessionConfig } from "../../../utils/session";
 
 const DEFAULT_REGION = "us-east-1"
@@ -6,7 +6,7 @@ export const DEFAULT_BUCKET_NAME = "app-5g-uploader"
 
 function getBucketName() {
   const { sessionIdentifier } = getSessionConfig()
-  return DEFAULT_BUCKET_NAME + sessionIdentifier
+  return DEFAULT_BUCKET_NAME + '-' + sessionIdentifier
 }
 
 function getBucketConfig() {
@@ -20,18 +20,25 @@ function createNewBucketCommand(bucketConfig) {
   return new CreateBucketCommand(bucketConfig);
 }
 
-function bucketExistCommand(bucketParams){
+function listBucketObjectsCommand(listParams){
+  return new ListObjectsCommand(listParams)
+}
+
+function bucketExistCommand(bucketParams) {
   return new HeadBucketCommand(bucketParams)
 }
 
 function configureS3Client(configs) {
+  if (!configs.region) {
+    configs.region = DEFAULT_REGION
+  }
   const s3Client = new S3Client(configs);
   return s3Client
 }
 
-async function sendS3Command(client, command){
+async function sendS3Command(client, command) {
   try {
-    await client.send(command)
+    return await client.send(command)
   } catch (error) {
     console.log('S3 API ERROR: ', error)
     throw error
@@ -42,7 +49,8 @@ export const awsS3Helper = {
   getBucketConfig,
   bucketExistCommand,
   createNewBucketCommand,
+  listBucketObjectsCommand,
   configureS3Client,
-  initiateS3Client: ({region, credentials}) => configureS3Client({region, credentials}),
+  initiateS3Client: ({ region, credentials }) => configureS3Client({ region, credentials }),
   sendS3Command
 }
