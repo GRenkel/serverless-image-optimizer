62633d100189293a12c02250487c17e1af2456b6
Renkel <guirenkel@gmail.com>
2023-06-09 10:10:51 -0300

    refac: improves skeleton layout

--- src/components/FileSkeletonCard.jsx
+++ src/components/FileSkeletonCard.jsx
@@ -5,10 +5,22 @@ export function FileSkeletonCard() {
   return (
     <Card
       hoverable
-      style={{ width: 250, height: 150, margin: '10px' }}
+      style={{ width: 250, height: 185, margin: '10px' }}
     >
-      <Skeleton.Avatar active shape="circle" size="large" />
-      <Skeleton paragraph={{ rows: 1 }} />
+      <div style={{ display: 'flex', maxHeight:150, flexDirection: 'column', alignItems: 'center', gap: 20 }}>
+        <Skeleton.Avatar active shape="circle" size="large" />
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%' }}>
+        <Skeleton.Input active={true} size={'small'}/>
+        </div>
+        <div style={{ display: 'flex', width: '100%', justifyContent: 'space-around' }}>
+          <Skeleton.Button
+            size={'default'}
+          />
+          <Skeleton.Button
+            size={'default'}
+          />
+        </div>
+      </div>
     </Card>
   )
 }
