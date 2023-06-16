643cdfe31cbb9028ac260eb426ded6f3e2c65f31
Renkel <guirenkel@gmail.com>
2023-06-16 11:06:36 -0300

    feat: handles login status using an Enumerator

--- src/services/aws/cognito/CognitoAPIHelper.js
+++ src/services/aws/cognito/CognitoAPIHelper.js
@@ -1,8 +1,13 @@
-import { CognitoUserAttribute } from "amazon-cognito-identity-js";
+import { AuthenticationDetails, CognitoUser, CognitoUserAttribute, } from "amazon-cognito-identity-js";
 import CognitoUserPool from "./CognitoUserPool";
+import EAuthStatus from './EAuthStatus.json'
 
 export const CognitoAPIHelper = {
-  
+
+  getAuthStatus: function (code) {
+    return EAuthStatus[code] !== undefined ? EAuthStatus[code] : EAuthStatus.failedToLogin
+  },
+
   getCognitoAttributesArrayFromObject: function (attributesObject) {
     let cognitoAttributes = []
     for (const attribute in attributesObject) {
@@ -26,5 +31,46 @@ export const CognitoAPIHelper = {
         }
       });
     });
+  },
+  userLogin: async function (email, password) {
+    const authData = {
+      Username: email,
+      Password: password
+    }
+    const authenticationDetails = new AuthenticationDetails(authData);
+
+    const userData = {
+      Username: email,
+      Pool: CognitoUserPool,
+    }
+
+    let cognitoUser = new CognitoUser(userData);
+
+    return new Promise((resolve, reject) => {
+      cognitoUser.authenticateUser(authenticationDetails, {
+        onSuccess: (result) => {
+          debugger
+          const jwtToken = result.getAccessToken().getJwtToken()
+          console.log('success authenticating', result);
+          resolve({ authStatus: EAuthStatus.isLogged, jwtToken })
+        },
+        onFailure: (error) => {
+          console.log('error authenticating', error);
+          reject({ authStatus: this.getAuthStatus(error.code), error })
+        },
+        newPasswordRequired: (userAttributes, requiredAttributes) => {
+          // User was signed up by an admin and must provide new
+          // password and required attributes, if any, to complete
+          // authentication.
+
+          // the api doesn't accept this field back
+          delete userAttributes.email_verified;
+          resolve({ authStatus: EAuthStatus.mustChangePassword, userAttributes })
+
+          // store userAttributes on global variable
+          // sessionUserAttributes = userAttributes;
+        }
+      });
+    })
   }
 }
