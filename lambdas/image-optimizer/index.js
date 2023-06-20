3f2ac12929482f80b497074bda4c9cb1fda68d4f
Renkel <guirenkel@gmail.com>
2023-06-20 10:24:15 -0300

    feat: adds optimizer lambda

---
+++ lambdas/image-optimizer/index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const sharp = require('sharp');
+const {basename, extname} = require('path')
+
+const S3 = new AWS.S3()
+
+module.exports.handle = async ({ Records: records }) => {
+  try {
+    await Promise.all(records.map(async record => {
+      const { key } = record.s3.object;
+
+      const image = await S3.getObject({
+        Bucket: process.env.bucket,
+        Key: key
+      }).promise()
+
+      const optimized = await sharp(image.Body)
+        .resize(1280, 720, { fit: 'inside', withoutEnlargement: true })
+        .toFormat('jpeg', { progressive: true, quality: 50 })
+        .toBuffer()
+
+      await S3.putObject({
+        Body: optimized,
+        Bucket: process.env.bucket,
+        ContentType: 'image/jpeg',
+        Key: `optimized/${basename(key, extname(key))}.jpeg`
+      }).promise()
+
+    }))
+
+    return {
+      statusCode: 301,
+      body: {
+
+      }
+    }
+  } catch (error) {
+    console.log(error)
+    return error
+  }
+};
