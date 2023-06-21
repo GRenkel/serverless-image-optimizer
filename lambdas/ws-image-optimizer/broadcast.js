fc1c20cce0747968549fb1047b1a38401dd1a8c6
Renkel <guirenkel@gmail.com>
2023-06-21 11:37:31 -0300

    refac: updates broadcast lambda to get connection from DynamoDB

--- lambdas/ws-image-optimizer/broadcast.js
+++ lambdas/ws-image-optimizer/broadcast.js
@@ -1,34 +1,49 @@
 const AWS = require('aws-sdk');
 
-exports.handler = async (event) => {
+exports.handler = async ({ Records: records }) => {
+
   const gatewayApi = new AWS.ApiGatewayManagementApi({
     apiVersion: '2018-11-29',
     endpoint: process.env.API_GATEWAY_WS_URL
   });
 
   try {
-    const scanResult = await new AWS.DynamoDB.DocumentClient().scan({ TableName: process.env.CONNECTIONS_TABLE }).promise();
-    console.log(scanResult)
-    const connections = scanResult.Items.map(item => item['connection-id']);
-    console.log(connections)
-    const broadcastMessage = 'Hello, everyone!';
+    await Promise.all(records.map(async record => {
+      const objectKey = record.s3.object.key
+      const userId = objectKey.split('/')[1]
 
-    await Promise.all(connections.map(async (connectionId) => {
+      console.log('Object being processed: ', objectKey)
+      console.log('User receiving notification: ', userId)
+      let connectionId = null
+
       try {
-        await gatewayApi.postToConnection({ ConnectionId: connectionId, Data: broadcastMessage }).promise();
+
+        const connectionItem = await new AWS.DynamoDB.DocumentClient().get(
+          {
+            TableName: process.env.CONNECTIONS_TABLE,
+            Key: {
+              'user-id':userId
+            }
+          }
+        ).promise();
+        connectionId = connectionItem.Item['connection-id']
+        console.log('Connection Item dynamo DB: ', connectionItem)
+        console.log('Connection receiving message: ', connectionId)
+        await gatewayApi.postToConnection({ ConnectionId: connectionId, Data: objectKey }).promise();
+
       } catch (error) {
-        console.log(error)
+        console.log('Error performing notification request: ', error)
       }
-    }));
+    }))
 
     return {
       statusCode: 200,
-      body: 'Broadcast sent successfully.'
+      body: 'Notification sent successfully.'
     };
   } catch (error) {
     return {
       statusCode: 500,
-      body: 'Error sending broadcast: ' + error.message
+      body: 'Error sending notification: ' + error.message
     };
   }
 };
